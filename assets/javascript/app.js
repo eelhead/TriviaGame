3564b94d638a0feb0c5f17447243de6b09c09f2a
Adam MIller <adammiller@Adams-MBP.domain>
2017-11-28 17:43:38 -0700

    questions added on js

---
+++ assets/javascript/app.js
@@ -0,0 +1,62 @@
+var triviaQuestions = [{
+	question: "This musician is often referred to as the fifth Beatle.",
+	answerList: ["Peter Framton", "Tom Jones", "Pete Best", "Chuck Yager"],
+	answer: 2
+},{
+	question: "How many keys are on most baby grand pianos?",
+	answerList: ["88", "102", "66", "122"],
+	answer: 0
+},{
+	question: "What was Pixar's first feature-length film that was released in 1995?",
+	answerList: ["Toy Story", "A Bug's Life", "Monster's Inc", "Finding Nemo"],
+	answer: 0
+},{
+	question: "Which one of Prince's songs reached highest on the music charts?",
+	answerList: ["Little Red Corvette", "1999", "When Dove's Cry", "Gold"],
+	answer: 2
+},{
+	question: "Who was the lead singer of the band Queen?",
+	answerList: ["Roger Clyne", "Jimmy Page", "Pete Townsand", "Freddie Mercury"],
+	answer: 3
+},{
+	question: "Which band released the song, Hey There Delilah?",
+	answerList: ["lain White T's ", "The Police", "Kiss", "Rainbow"],
+	answer: 0
+},{
+	question: "All Shook Up is a song that topped the U.S. Billboard Hot 100 in 1957. Who was the singer?",
+	answerList: ["Tom Petty", "Elvis Presley", "Edger Allan Poe", "Jerry Lee Lewis"],
+	answer: 1
+},{
+	question: "What musician won the Nobel Prize for Literature in 2016?",
+	answerList: ["Kurt Cobain", "Neal Young", "Bob Dylan", "Dave Mustaine"],
+	answer: 2
+},{
+	question: "What American singer-songwriter wrote and first recorded the song Blue Suede Shoes in 1955?",
+	answerList: ["Elvis Presley", "Carl Perkins", "Nate King Cole", "BB King"],
+	answer: 1
+},{
+	question: "Hallelujah is a song written by which Canadian recording artist?",
+	answerList: ["Joni Mitchell", "Alanis Morissette", "Neil Young", "Leonard Cohen"],
+	answer: 3
+},{
+	question: "What famous female singer died of alcohol poisoning in 2011 at the age of 27?",
+	answerList: ["Amy Winehouse", "Tommy Keene", "Dan Peek", "Jani Lane"],
+	answer: 0
+},{
+	question: "Which famous American musician was fatally shot by his father on April 1, 1984?",
+	answerList: ["Harmonica Slim", "Marvin Gaye", "Lenny Breau", "Steve Goodman"],
+	answer: 1
+},{
+	question: "In 2012, Psy got the record for most YouTube views ever, passing what artist who previously held the record?",
+	answerList: ["Snoop Dog", "Prince", "Justin Timberlake", "Justin Bieber"],
+	answer: 3
+},{
+	question: "Who is the most successful UK solo artist in the US?",
+	answerList: ["Elton John", "Amy Winehouse", "Goerge Michael", "Carl Carlton"],
+	answer: 0
+},{
+	question: "In the original Jackson family line up, how many brothers were there?",
+	answerList: ["6", "4", "5", "3"],
+	answer: 2
+}];
+
