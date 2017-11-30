7f3fca24e7b7fd87ddb1cdb958eeb494887f2ded
Adam MIller <adammiller@Adams-MBP.domain>
2017-11-29 17:42:44 -0700

    added notes to code after watching the career homework videos

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+// the questions begin
 var triviaQuestions = [{
 	question: "This musician is often referred to as the fifth Beatle.",
 	answerList: ["Peter Framton", "Tom Jones", "Pete Best", "Chuck Yager"],
@@ -60,6 +61,8 @@ var triviaQuestions = [{
 	answer: 2
 }];
 
+// so now these are variables for the array and all that I want to happen taking the trivia game
+//as you can see it is the main elements of the trivia game
 var gifArray = ['question1', 'question2', 'question3', 'question4', 'question5', 'question6', 'question7', 'question8', 'question9', 'question10', 'question11', 'question12', 'question13', 'question14', 'question15'];
 var currentQuestion; 
 var correctAnswer; 
@@ -70,6 +73,7 @@ var time;
 var answered;
 var userselect;
 
+// so this is the messages you'll get throughout the game
 var messages = {
 	correct: "Yes, correct!",
 	incorrect: "Nope, that's not it.",
