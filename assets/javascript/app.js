8d2808c219016feee25313b48f8a97e3409f0a19
Adam MIller <adammiller@Adams-MBP.domain>
2017-12-02 10:17:58 -0700

    should all be working now

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -21,7 +21,7 @@ var triviaQuestions = [{
 	answer: 3
 },{
 	question: "Which band released the song, Hey There Delilah?",
-	answerList: ["lain White T's ", "The Police", "Kiss", "Rainbow"],
+	answerList: ["Plain White T's ", "The Police", "Kiss", "Rainbow"],
 	answer: 0
 },{
 	question: "All Shook Up is a song that topped the U.S. Billboard Hot 100 in 1957. Who was the singer?",
@@ -79,4 +79,116 @@ var messages = {
 	incorrect: "Nope, that's not it.",
 	endtime: "Out of time!!!!",
 	finished: "OK! Let's check out your score!"
+}
+
+//will hide start button when clicked for a new game 
+$('#startBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+//will hide the startover button and create a new game
+$('#startOverBtn').on('click', function(){
+	$(this).hide();
+	newGame();
+});
+
+//when starting a new game will give you current scores also keep a running tally
+function newGame(){
+	$('#finalMessage').empty();
+	$('#correctAnswers').empty();
+	$('#incorrectAnswers').empty();
+	$('#unanswered').empty();
+	currentQuestion = 0;
+	correctAnswer = 0;
+	incorrectAnswer = 0;
+	unanswered = 0;
+	newQuestion();
+}
+
+//this prompts new question and states correct answer when answered correctly 
+function newQuestion(){
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	answered = true;
+	
+	//sets up new questions & answerList
+	$('#currentQuestion').html('Question #'+(currentQuestion+1)+'/'+triviaQuestions.length);
+	$('.question').html('<h2>' + triviaQuestions[currentQuestion].question + '</h2>');
+	for(var i = 0; i < 4; i++){
+		var choices = $('<div>');
+		choices.text(triviaQuestions[currentQuestion].answerList[i]);
+		choices.attr({'data-index': i });
+		choices.addClass('thisChoice');
+		$('.answerList').append(choices);
+	}
+	countdown();
+	//clicking an answer will pause the time and setup answerPage
+	$('.thisChoice').on('click',function(){
+		userSelect = $(this).data('index');
+		clearInterval(time);
+		answerPage();
+	});
+}
+//this is the timer start for each new question, answering the right question stops timer 
+function countdown(){
+	seconds = 15;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	answered = true;
+	//sets timer to go down
+	time = setInterval(showCountdown, 1000);
+}
+//shows the timer
+function showCountdown(){
+	seconds--;
+	$('#timeLeft').html('<h3>Time Remaining: ' + seconds + '</h3>');
+	if(seconds < 1){
+		clearInterval(time);
+		answered = false;
+		answerPage();
+	}
+}
+
+function answerPage(){
+	$('#currentQuestion').empty();
+	$('.thisChoice').empty(); //Clears question page
+	$('.question').empty();
+
+	var rightAnswerText = triviaQuestions[currentQuestion].answerList[triviaQuestions[currentQuestion].answer];
+	//checks to see correct, incorrect, or unanswered
+	if((userSelect == rightAnswerIndex) && (answered == true)){
+		correctAnswer++;
+		$('#message').html(messages.correct);
+	} else if((userSelect != rightAnswerIndex) && (answered == true)){
+		incorrectAnswer++;
+		$('#message').html(messages.incorrect);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+	} else{
+		unanswered++;
+		$('#message').html(messages.endTime);
+		$('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
+		answered = true;
+	}
+	
+	if(currentQuestion == (triviaQuestions.length-1)){
+		setTimeout(scoreboard, 3000)
+	} else{
+		currentQuestion++;
+		setTimeout(newQuestion, 3000);
+	}	
+}
+//the running tally of the games you've played as well as correct or incorrect questions answered
+function scoreboard(){
+	$('#timeLeft').empty();
+	$('#message').empty();
+	$('#correctedAnswer').empty();
+	$('#gif').empty();
+	//the messages desplayed throughout the game
+	$('#finalMessage').html(messages.finished);
+	$('#correctAnswers').html("Correct Answers: " + correctAnswer);
+	$('#incorrectAnswers').html("Incorrect Answers: " + incorrectAnswer);
+	$('#unanswered').html("Unanswered: " + unanswered);
+	$('#startOverBtn').addClass('reset');
+	$('#startOverBtn').show();
+	$('#startOverBtn').html('Start Over?');
 }
